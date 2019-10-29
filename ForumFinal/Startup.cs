133754fbfde88c2a9e70f9ab2543634099e81a97
Kevin Stone <ruca6616@gmail.com>
2019-10-28 19:11:06 -0700

    Implmented methods that query the database.

--- ForumFinal/Startup.cs
+++ ForumFinal/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ForumFinal.Data;
+using MusicianForums.Data;
+using MusicianForums.Services;
 
 namespace ForumFinal
 {
@@ -28,6 +30,8 @@ namespace ForumFinal
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddScoped<IForum, ForumService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- MusicianForums.Services/ForumService.cs
+++ MusicianForums.Services/ForumService.cs
@@ -5,6 +5,7 @@ using MusicianForums.Data;
 using MusicianForums.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MusicianForums.Services
@@ -27,6 +28,10 @@ namespace MusicianForums.Services
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Return all Forums with their associated posts.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Forum> GetAll()
         {
 
@@ -39,9 +44,21 @@ namespace MusicianForums.Services
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Returns a single Forum corresponding to the Id passed 
+        /// in as a parameter.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Forum GetById(int id)
         {
-            throw new NotImplementedException();
+            var forum = _context.Forums.Where(forum => forum.Id == id)
+                .Include(forum => forum.Posts).ThenInclude(post => post.User)
+                .Include(forum => forum.Posts).ThenInclude(post => post.Replies)
+                .ThenInclude(r => r.User)
+                .FirstOrDefault();
+
+            return forum;
         }
 
         public Task UpdateForumDescription(int forumId, string newDescription)
