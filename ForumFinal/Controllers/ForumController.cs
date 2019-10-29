ad06b7d21c5f3e617027c9f3598adfeae40c462f
Kevin Stone <ruca6616@gmail.com>
2019-10-28 19:11:53 -0700

    Created models and views to display seed data.

---
+++ ForumFinal/Controllers/ForumController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ForumFinal.Models.Forum;
+using Microsoft.AspNetCore.Mvc;
+using MusicianForums.Data;
+using MusicianForums.Data.Models;
+
+namespace ForumFinal.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+
+        /// <summary>
+        /// Returns View with list of all Forums.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            var forums = _forumService
+                .GetAll()
+                .Select(forum => new ForumListingModel { 
+                    Id = forum.Id,
+                    Name = forum.Title,
+                    Description = forum.Description
+                });
+
+            var model = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+
+            return View(model);
+        }
+
+
+        public IActionResult Topic(int id)
+        {
+            var forum = _forumService.GetById(id);
+
+
+        }
+
+    }
+}
---
+++ ForumFinal/Models/Forum/ForumIndexModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumFinal.Models.Forum
+{
+    public class ForumIndexModel
+    {
+
+        public IEnumerable<ForumListingModel> ForumList { get; set; }
+
+
+    }
+}
